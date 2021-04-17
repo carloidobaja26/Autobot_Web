475fb786889374994b6f54666ce8d60c234b8c0d
carlo.i.baja <carlo.i.baja@accenture.com>
2021-04-17 22:04:03 +0800

    inital-folder-structure

---
+++ src/components/front-trading/Front-Trading.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import './Front-Trading.css';
+
+const FrontTrading = () => {
+    return (
+        <div>
+            <p>Hello World</p>
+        </div>
+    );
+}
+
+export default FrontTrading;
